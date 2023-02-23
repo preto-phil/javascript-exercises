50c0e97486b0b2200f2fab594d1dbc75b1466c88
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-23 19:28:11 +0200

    Calculator exercise finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(x) {
-  const y = (x - 32) / 9 * 5;
+  let y = (x - 32) / 9 * 5;
   y = Math.round(y);
   return y;
 };
 
 const ctof = function(x) {
-  const y = (x / 9 * 5) + 32;
+  let y = (x / 9 * 5) + 32;
   y = Math.round(y);
   return y;
 };

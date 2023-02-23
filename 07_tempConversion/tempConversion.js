b0589be46adbb7e38546e035b3bd634c031e90b5
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-23 18:12:07 +0200

    tempConversion finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  const y = (x - 32) / 9 * 5;
+  y = Math.round(y);
+  return y;
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+  const y = (x / 9 * 5) + 32;
+  y = Math.round(y);
+  return y;
 };
 
 // Do not edit below this line
